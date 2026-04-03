a60be9f25e5bbc4aed7efc88d5bfbd969cc57492
Yebin0319 <kuyebin@naver.com>
2026-04-03 20:01:53 +0900

    세션용_수정

--- src/main/java/com/likelion/session/dto/BoardCreateRequest.java
+++ src/main/java/com/likelion/session/dto/BoardCreateRequest.java
@@ -11,7 +11,7 @@ import lombok.Setter;
 @AllArgsConstructor
 public class BoardCreateRequest {
     // 넘겨주고 싶은 정보: 제목(title), 내용(content), 작성자(writer)
-    private String title;
-    private String content;
-    private String writer;
+    private String ;
+    private String ;
+    private String ;
 }
