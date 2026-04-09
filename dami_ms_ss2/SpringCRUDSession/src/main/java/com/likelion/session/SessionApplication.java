9cd3d8ea6211a885e312925e5629aa76012bf14e
dami6326 <dami6326@gmail.com>
2026-04-09 19:12:08 +0900

    제발제발

---
+++ dami_ms_ss2/SpringCRUDSession/src/main/java/com/likelion/session/SessionApplication.java
@@ -0,0 +1,13 @@
+package com.likelion.session;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication //이 프로젝트를 실행하는 메인 설정 클래스라는 걸 알림.
+public class SessionApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SessionApplication.class, args);
+	}
+
+}
